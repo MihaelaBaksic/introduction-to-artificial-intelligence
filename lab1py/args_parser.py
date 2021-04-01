53e91003ba3bf8bca4587c0dffb23530601a7356
MihaelaBaksic <mihaela.baksic@gmail.com>
2021-04-01 21:26:37 +0200

    Fixed argument passing

--- lab1py/args_parser.py
+++ lab1py/args_parser.py
@@ -4,8 +4,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--alg', type=str)
 parser.add_argument('--ss', type=str)
 parser.add_argument('--h', type=str)
-parser.add_argument('--check_optimistic', action='store_true')
-parser.add_argument('--check_consistent', action='store_true')
+parser.add_argument('--check-optimistic', action='store_true')
+parser.add_argument('--check-consistent', action='store_true')
 
 args = parser.parse_args()
 
